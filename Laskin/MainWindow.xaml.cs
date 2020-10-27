d2c981b9076e243d53155053cc634bd790b3b640
HeneDev <44865736+HeneDev@users.noreply.github.com>
2020-10-27 14:32:10 +0200

    initial commit

---
+++ Laskin/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laskin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTimes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPosNeg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPercent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1x_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPower_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSqrt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
